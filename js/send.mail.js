11e680271ede02f9c3ba3ab21dfca881beea886e
Jordie LUTUNDULA <J.LUTUNDULA@bracongo.local>
2022-04-13 15:11:17 +0200

    prevent

--- js/send.mail.js
+++ js/send.mail.js
@@ -5,18 +5,19 @@ const name = document.getElementsByName('name');
 const object = document.getElementsByName('object');
 const message = document.getElementsByName('message');
 
-const sendMessage = () => {
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "redilutunx@gmail.com",
-        Password : "BE17B5CAB4735C788D53508CDFDF35C0F8AC",
-        To : 'redilutunx@gmail.com',
-        From : email,
-        Subject : object,
-        Body : message
-    }).then(
-      message => alert("message envoyé avec succès")
-    );
+const sendMessage = (e) => {
+  e.preventDefault();
+  Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "redilutunx@gmail.com",
+      Password : "BE17B5CAB4735C788D53508CDFDF35C0F8AC",
+      To : 'redilutunx@gmail.com',
+      From : email,
+      Subject : object,
+      Body : message
+  }).then(
+    message => alert("message envoyé avec succès")
+  );
 }
 
 contactForm.addEventListener("submit", sendMessage);
